93ac4acc60281a15995e923215fcc3d33630878f
fjreyesm <franklin.reyes.2003@gmail.com>
2022-09-23 19:09:56 +0200

    conf inicial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Tetris</h1>
+
+      <button onClick={() => setCount((count) => count + 1)}>
+        Play {count}
+      </button>
+    </div>
+  );
+}
+
+export default App;
